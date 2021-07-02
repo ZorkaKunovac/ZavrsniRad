4bdc76eacf07a5daa41dc67b64707c61e2102880
Zorka Kunovac <61622729+ZorkaKunovac@users.noreply.github.com>
2021-07-02 17:48:19 +0200

    ApiService

---
+++ GamingHub2.MobileApp/GamingHub2.MobileApp/APIService.cs
@@ -0,0 +1,109 @@
+﻿using Flurl.Http;
+using GamingHub2.Model;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GamingHub2.MobileApp
+{
+    public class APIService
+    {
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+
+        private string _route = null;
+#if DEBUG
+        private string _apiURL = "https://localhost:5001/api/";
+#endif
+#if RELEASE
+        private string _apiURL = "https://mywebsite/api/";
+#endif
+        //public string endpoint = $"{_apiURL}";
+        public APIService(string route) //Kontroler
+        {
+            _route = route;
+        }
+
+        public async Task<T> Get<T>(object searchRequest = null)
+        {
+            var url = $"{_apiURL}/{_route}";
+            try
+            {
+                if (searchRequest != null)
+                {
+                    url += "?";
+                    url += await searchRequest?.ToQueryString();
+                }
+
+                return await url.WithBasicAuth(Username, Password).GetJsonAsync<T>();
+            }
+            catch (FlurlHttpException ex)
+            {
+                if (ex.StatusCode == (int)HttpStatusCode.Unauthorized)
+                {
+                     await Application.Current.MainPage.DisplayAlert("Greska","Niste autorizovani","OK");
+                }
+                if (ex.StatusCode == (int)HttpStatusCode.Forbidden)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Nemate privilegije", "Pristup zabranjen", "OK");
+                }
+                return default(T);
+            }
+        }
+        public async Task<T> GetById<T>(object id)
+        {
+            var url = $"{_apiURL}/{_route}/{id}";
+            return await url.WithBasicAuth(Username, Password).GetJsonAsync<T>();
+        }
+        public async Task<T> Insert<T>(object request)
+        {
+            var url = $"{_apiURL}/{_route}";
+            //var result = await url.PostJsonAsync(request).ReceiveJson<T>();
+            //return result;
+            try
+            {
+                return await url.WithBasicAuth(Username, Password).PostJsonAsync(request).ReceiveJson<T>();
+            }
+            catch (FlurlHttpException ex)
+            {
+                if (ex.StatusCode == (int)HttpStatusCode.Unauthorized)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Greska", "Niste autorizovani", "OK");
+                }
+                if (ex.StatusCode == (int)HttpStatusCode.Forbidden)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Nemate privilegije", "Pristup zabranjen", "OK");
+                }
+
+                return default(T);
+
+                // return default;
+            }
+        }
+
+        public async Task<T> Update<T>(object id, object request)
+        {
+            try
+            {
+                var url = $"{_apiURL}/{_route}/{id}";
+
+                return await url.WithBasicAuth(Username, Password).PutJsonAsync(request).ReceiveJson<T>();
+            }
+            catch (FlurlHttpException ex)
+            {
+                if (ex.StatusCode == (int)HttpStatusCode.Unauthorized)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Greska", "Niste autorizovani", "OK");
+                }
+                if (ex.StatusCode == (int)HttpStatusCode.Forbidden)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Nemate privilegije", "Pristup zabranjen", "OK");
+                }
+                return default(T);
+            }
+        }
+    }
+}
